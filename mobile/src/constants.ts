482285f3f82aa5fc2f0fc39575b9981c92e81395
Arda Irmak <ardairmak2003@gmail.com>
2025-04-03 00:31:31 +0300

    Add gender field to onboarding stats screen with complete UI implementation

--- mobile/src/constants.ts
+++ mobile/src/constants.ts
@@ -1,3 +1,5 @@
 // Authentication constants
-export const AUTH_TOKEN_KEY = "auth_token";
-export const USER_DATA_KEY = "user_data";
+export const AUTH_TOKEN_KEY = "@auth_token";
+export const USER_DATA_KEY = "@user_data";
+
+// API endpoints and other constants can be added here
